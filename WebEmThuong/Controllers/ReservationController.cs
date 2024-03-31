273a24743968c41e4adc1376372b709acca830e5
TUANTRAN <tuanpvh10@gmail.com>
2024-03-31 22:54:02 +0700

    feat: done  UI for  user

---
+++ WebEmThuong/Controllers/ReservationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebEmThuong.Controllers
+{
+    public class ReservationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
