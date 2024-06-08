e6a1c45211357c5756ced99a7c3e4b55405d7d0c
TUANTRAN <tuanpvh10@gmail.com>
2024-06-08 14:09:28 +0700

    feat: done view for categories

--- WebEmThuong/Controllers/ShopController.cs
+++ WebEmThuong/Controllers/ShopController.cs
@@ -12,10 +12,17 @@ namespace WebEmThuong.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int categoryId)
         {
             var catagories = await _context.Category.OrderBy(c=>c.Sort).ToListAsync();
-            var productions = await _context.Productions.OrderBy(p => p.Sort).ToListAsync();
+            var productions = new List<Production>();
+
+            if (categoryId != 0)
+            {
+                productions = await _context.Productions.Where(p => p.CatagoryId == categoryId).OrderBy(p => p.Sort).ToListAsync();
+            }
+
+            productions = await _context.Productions.OrderBy(p => p.Sort).ToListAsync();
 
             var shopViewModel = new ShopViewModel
             {
